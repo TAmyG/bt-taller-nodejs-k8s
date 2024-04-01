c98b55830b4d890b5484c21bf6de3bdbf6dea37b
tamy <tamy.vivas@gmail.com>
2024-04-01 09:22:19 -0600

    feat: get posts

--- blog-bt/posts/index.js
+++ blog-bt/posts/index.js
@@ -15,17 +15,16 @@ const posts = {};
 app.post('/posts/create', (req, res) => {
     const id = randomBytes(4).toString('hex');
     const {titulo} = req.body;
-
     posts[id]= {
         id: id,
         titulo: titulo
     }
-
     res.send(posts[id]);
 })
 
-
-
+app.get('/posts', (req, res)=>{
+    res.send(posts)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
