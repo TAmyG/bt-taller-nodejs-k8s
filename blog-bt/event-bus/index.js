35c32f22cec922e5eabf48f5a2efe29e6971661e
tamy <tamy.vivas@gmail.com>
2024-04-01 10:04:18 -0600

    feat: event bus

---
+++ blog-bt/event-bus/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { default: axios } = require('axios')
+
+const app = express()
+app.use(bodyParser.json())
+
+
+const port = 4005
+const eventos = [];
+
+app.post('/events', (req, res)=>{
+    const evento = req.body;
+
+    eventos.push(evento);
+    console.log('Event Bus: ', evento.type)
+
+    // Logica para enviar eventos a los consumers
+    axios.post('http://localhost:4000/events', evento)
+
+    .catch(err=>{
+        console.error(err.message)
+    })
+
+    res.send({status: 'OK'});
+});
+
+app.get('/events', (req, res)=>{
+    res.send(eventos);
+})
+
+app.listen(port, () => {
+    console.log(`Event Bus app listening on port ${port}`)
+})
--- blog-bt/posts/index.js
+++ blog-bt/posts/index.js
@@ -9,21 +9,38 @@ app.use(bodyParser.json())
 app.use(cors())
 
 
-const port = 8000
+const port = 4000
 const posts = {};
 
-app.post('/posts/create', (req, res) => {
+app.post('/posts/create', async(req, res) => {
     const id = randomBytes(4).toString('hex');
     const {titulo} = req.body;
     posts[id]= {
         id: id,
         titulo: titulo
     }
+
+    //Notificar al MQ que se acaba de crear un post
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreado',
+        data: {
+            id: id,
+            titulo: titulo
+        }
+    }). catch(err=>{
+        console.error(err)
+    })
     res.send(posts[id]);
 })
 
 app.get('/posts', (req, res)=>{
     res.send(posts)
+})
+
+
+app.post('/events', (req, res)=>{
+    console.log('Evento recibido: ', req.body.type)
+    res.send({});
 })
 
 app.listen(port, () => {
