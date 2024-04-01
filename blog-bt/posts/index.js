e0f117977bff23727619592df95516148c20aa93
tamy <tamy.vivas@gmail.com>
2024-04-01 09:08:40 -0600

    feat: post ms

---
+++ blog-bt/posts/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const {randomBytes} = require('crypto')
+const cors = require('cors')
+const { default: axios } = require('axios')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+
+const port = 8000
+const posts = {};
+
+app.post('/posts/create', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const {titulo} = req.body;
+
+    posts[id]= {
+        id: id,
+        titulo: titulo
+    }
+
+    res.send(posts[id]);
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
